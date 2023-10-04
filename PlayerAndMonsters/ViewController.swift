44ef319d0ee1da0832ea1f70a54ec8cb2bbb5398
Nikita <windowsmax43@gmail.com>
2023-10-04 23:13:13 +0700

    Initial Commit

---
+++ PlayerAndMonsters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayerAndMonsters
+//
+//  Created by quest_01 on 04/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
